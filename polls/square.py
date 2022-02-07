3bad302a6acce5ad3e6826f35afd458d7f145383
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2022-02-07 12:08:52 +0900

    django unittest の実装

---
+++ polls/square.py
@@ -0,0 +1,5 @@
+class Square():
+
+    def get_area(self, x):
+        """引数 x を受け取って、 x の2乗を返す"""
+        return x * x
--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from polls.square import Square
 
-# Create your tests here.
+
+class SquareTest(TestCase):
+
+    def test_get_area(self):
+        square = Square()
+
+        x = 1
+        expected = 1
+        actual = square.get_area(x)
+        self.assertEqual(actual, expected)
+        
+        x = 2
+        expected = 4
+        actual = square.get_area(x)
+        self.assertEqual(actual, expected)
