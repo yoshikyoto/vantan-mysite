22ad4ec1e5b31944b3602e58dbd4cc892c94aa1b
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2022-02-14 10:56:39 +0900

    mock

--- polls/square.py
+++ polls/square.py
@@ -1,5 +1,17 @@
+class Number():
+     """数字を扱うクラス"""
+ 
+     def get_square(self, x):
+         """2乗の値を返す"""
+         return 10000
+
+
 class Square():
+    """正方形を表すクラス"""
+
+    def __init__(self, number = Number()):
+        self.__number = number
 
     def get_area(self, x):
-        """引数 x を受け取って、 x の2乗を返す"""
-        return x * x
+        """面積を求める"""
+        return self.__number.get_square(x)
--- polls/tests.py
+++ polls/tests.py
@@ -1,18 +1,17 @@
 from django.test import TestCase
+from unittest.mock import MagicMock
 from polls.square import Square
-
+from django.urls import reverse
 
 class SquareTest(TestCase):
 
     def test_get_area(self):
-        square = Square()
+        number_mock = MagicMock()
+        number_mock.get_square.return_value = 1
+
+        square = Square(number = number_mock)
 
         x = 1
         expected = 1
-        actual = square.get_area(x)
-        self.assertEqual(actual, expected)
-        
-        x = 2
-        expected = 4
         actual = square.get_area(x)
         self.assertEqual(actual, expected)
